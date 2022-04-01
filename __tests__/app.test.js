28b2d2e9cd307cd670941f1851cae5a1c94050c4
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-04-01 12:33:51 -0700

    updated second test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -15,27 +15,20 @@ describe('gitty routes', () => {
   });
 
   it('redirects to github oauth page when logging in', async () => {
-    const req = await request(app)
+    const res = await request(app)
       .get('/api/v1/github/login');
 
-    expect(req.header.location).toMatch('https://github.com/login/oauth/authorize?client_id=undefined&scope=user&redirect_uri=undefined');
+    expect(res.header.location).toMatch('https://github.com/login/oauth/authorize?client_id=undefined&scope=user&redirect_uri=undefined');
   });
 
 
-  it('should callback URI for Github to redirect to after log in', async () => {
-    const req = await request
+  it('should redirect to posts when logged in', async () => {
+    const res = await request
       .agent(app)
       .get('/api/v1/github/login/callback?code=42')
       .redirects(1);
 
-    expect(req.body).toEqual({
-      id: expect.any(String),
-      username: 'Ernie',
-      email: 'meow@meow.meow',
-      avatar: expect.any(String),
-      iat: expect.any(Number),
-      exp: expect.any(Number)
-    });
+    expect(res.req.path).toEqual('/api/v1/posts');
 
   });
 
